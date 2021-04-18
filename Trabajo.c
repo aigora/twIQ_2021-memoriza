bab9d8bc914e4fdcab3e9eab3200a8601a032290
Javierelmasblanco <60717326+Javierelmasblanco@users.noreply.github.com>
2021-04-18 19:04:15 +0200

    Update Trabajo.c

--- Trabajo.c
+++ Trabajo.c
@@ -1,1 +1,114 @@
-HOLA Q HASE
+# include<stdio.h>
+# include<string.h>
+
+int main(){
+	
+	printf("    -------------------------------    \n");
+	printf("    -------------------------------    \n");
+	printf("    -----------MEMORIZA------------    \n");
+	printf("    -------------------------------    \n");
+	printf("    -------------------------------    \n");
+	
+	int opcion=0;
+	int temas=0;
+	int login=0;
+	
+	printf("Bienvenido al juego de memoriza, donde usted podra poner a prueba su capacidad para retener informacion.\n");
+	printf("\n");
+	
+	printf("Elija una opcion para continuar.\n");
+	printf("   1-Registrarse.\n");
+    printf("   2-Iniciar sesion.\n");
+	printf("   3-Como se juega.\n");
+	printf("   4-Salir.\n");
+	scanf("%d", &login);
+	
+	switch(login){
+		
+		case 1:
+			
+			
+			
+			break;
+			
+		case 2:
+			
+			
+			
+			break;
+			
+		case 3:
+			
+			
+			
+			break;
+			
+		case 4:
+			
+			
+			
+			break;
+	}
+	
+	
+	printf("A continuacion elegira el modo que desea jugar.\n");
+	printf("\n");
+	
+	printf("Opcion 1- Todos los temas.\n");
+	printf("Opcion 2- Numeros.\n");
+	printf("Opcion 3- Tema concreto.\n");
+	scanf("%d", &opcion);
+	
+	switch(opcion){
+	
+	    case 1:
+	    	
+	    	
+	    	break;
+	    
+	    case 2:
+	    	
+	    	
+	    	break;
+	    	
+	    case 3:
+	    	
+	        printf("Eliga un tema entre los siguientes.\n");
+	    	printf("Tema 1- Animales.\n");
+	        printf("Tema 2- Comidas.\n");
+	        printf("Tema 3- Enfermedades.\n");
+	        printf("Tema 4- Paises.\n");
+	        scanf("%d", &temas);
+	        
+	        switch(temas){
+	        	
+	        	case 1:
+	        		
+	        		
+	        		break;
+	        	
+	        	case 2:
+	        		
+	        		
+	        		break;
+	        		
+	        	case 3:
+	        		
+	        		
+	        		break;
+	        		
+	        	case 4:
+	        		
+	        		
+	        		break;
+	        		
+			}
+	        
+	        break;
+	    
+    }
+	    
+    
+}
+
+
